20f484bd4085f1945c40ad3675f542c0242013f8
chesnykh <bigkwest@mail.ru>
2022-09-06 10:31:43 +0300

    Criate migration

---
+++ FoodStoreApp/Controllers/CaregoryCaontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodStoreApp.Controllers
+{
+    public class CaregoryCaontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
